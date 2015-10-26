0d1583112eb34a4c032668f5a5c0a818d6c153ed
Cami Rodriguez <camiepisode@outlook.com>
2015-10-26 01:38:18 -0500

    Getting dynamic form

--- WebApp/WebApp/Controllers/RequirementController.cs
+++ WebApp/WebApp/Controllers/RequirementController.cs
@@ -1,13 +1,37 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+using MongoDB.Driver.GridFS;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using WebApp.Models.Entities;
+using WebApp.Properties;
 
 namespace WebApp.Controllers
 {
     public class RequirementController : Controller
     {
+        private MongoClient mongoClient;
+        private IMongoDatabase KonradRequirementsDatabase;
+        private IMongoCollection<BsonDocument> usersCollection;
+        private IMongoCollection<BsonDocument> requirementsCollection;
+        private IMongoCollection<BsonDocument> formsCollection;
+
+        public RequirementController()
+        {
+            mongoClient = new MongoClient(Settings.Default.MongoDBConnectionString);
+
+            KonradRequirementsDatabase = mongoClient.GetDatabase("KonradRequirements");
+            usersCollection = KonradRequirementsDatabase.GetCollection<BsonDocument>("Users");
+            requirementsCollection = KonradRequirementsDatabase.GetCollection<BsonDocument>("Requirements");
+            formsCollection = KonradRequirementsDatabase.GetCollection<BsonDocument>("Forms");
+        }
+
         /// <summary>
         /// Get all requirements
         /// </summary>
@@ -36,6 +60,27 @@ namespace WebApp.Controllers
         public ActionResult ModifyForm()
         {
             return View();
+        }
+
+        public async Task<ActionResult> GetTemplateByName(string name = "Default")
+        {
+            if (!String.IsNullOrWhiteSpace(name))
+            {
+                var filter = Builders<BsonDocument>.Filter.Eq("Name", name);
+                var document = await formsCollection.Find(filter).FirstOrDefaultAsync();
+
+                if (document != null)
+                {
+                    Form form = BsonSerializer.Deserialize<Form>(document);
+                    Response.StatusCode = (int)HttpStatusCode.OK;
+                    return Json(form, JsonRequestBehavior.AllowGet);
+
+                }
+                Response.StatusCode = (int)HttpStatusCode.NotFound;
+                return Json(new { Message = "La plantilla que está buscando no se encuentra." }, JsonRequestBehavior.AllowGet);
+            }
+            Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            return Json(new { Message = "Por favor indique qué plantilla desea modificar" }, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ WebApp/WebApp/Models/Entities/Form.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp.Models.Entities
+{
+    public class Form
+    {
+        [BsonId]
+        ObjectId _id { get; set; }
+        public string Name { get; set; }
+        public string Template { get; set; }
+    }
+}
